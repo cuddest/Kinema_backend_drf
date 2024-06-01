b332a0b2a11ece1683a4cb07819e131842d139c4
cuddest <142700499+cuddest@users.noreply.github.com>
2024-06-01 09:13:48 +0100

    nc

--- kinema/review/models.py
+++ kinema/review/models.py
@@ -6,7 +6,6 @@ class review(models.Model):
     user = models.ForeignKey("cinephile.User", on_delete=models.CASCADE)
     description = models.TextField()
     rating = models.IntegerField()
-    movie = models.ForeignKey("movies.Movie", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
